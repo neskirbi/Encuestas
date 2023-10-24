d533f4b2237a9273ff3bac6969ec504296acf00b
Raul <neskirbi@gmail.com>
2023-10-24 15:06:40 -0600

    Correccion url de bosrra formulario

--- routes/web.php
+++ routes/web.php
@@ -915,7 +915,7 @@ Route::resource('formularios','App\Http\Controllers\Uia\FormularioController');
 
 Route::get('EliminarFormulario/{id}','App\Http\Controllers\Uia\FormularioController@EliminarFormulario');
 
-Route::get('DestroyFormualario/{id}','App\Http\Controllers\Uia\FormularioController@DestroyFormualario');
+Route::get('DestroyFormulario/{id}','App\Http\Controllers\Uia\FormularioController@DestroyFormulario');
 
 Route::get('EliminarEncuesta/{id}','App\Http\Controllers\Uia\EncuestaController@EliminarEncuesta'); 
 Route::get('DestroyEncuesta/{id}','App\Http\Controllers\Uia\EncuestaController@DestroyEncuesta');
